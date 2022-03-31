b55708483b1878681e65090aa87d1ecadf6af8ea
Victor Manuel Damian Barron <vdamianb@emeal.nttdata.com>
2022-03-30 19:14:20 -0600

    implementando endpoint email cliente

---
+++ Services/MailService.cs
@@ -0,0 +1,48 @@
+﻿using aljuvifoods_webapi.Services.Contracts;
+using System.Net;
+using System.Net.Mail;
+
+namespace aljuvifoods_webapi.Services
+{
+    public abstract class MailService:IMailService
+    {
+        private SmtpClient smtpClient { get; set; }
+        protected string senderMail { get; set; }
+        protected string password { get; set; }
+        protected string host { get; set; }
+        protected int port { get; set; }
+        protected bool ssl { get; set; }
+
+        protected void InitSmtp()
+        {
+            smtpClient = new SmtpClient();
+            smtpClient.Credentials = new NetworkCredential(senderMail, password);
+            smtpClient.Host = host;
+            smtpClient.Port = port;
+            smtpClient.EnableSsl = ssl;
+        }
+
+        public async Task Send(string subject, string body, List<string> recipien)
+        {
+            var msg = new MailMessage();
+            try
+            {
+                msg.From = new MailAddress(senderMail);
+                foreach (string mail in recipien)
+                {
+                    msg.To.Add(mail);
+                }
+                msg.Subject = subject;
+                msg.Body = body;
+                msg.Priority = MailPriority.Normal;
+                smtpClient.Send(msg);
+            }
+            catch (Exception ex) { }
+            finally
+            {
+                msg.Dispose();
+                smtpClient.Dispose();
+            }
+        }
+    }
+}
---
+++ Services/SupportMail.cs
@@ -0,0 +1,15 @@
+﻿namespace aljuvifoods_webapi.Services
+{
+    public class SupportMail:MailService
+    {
+        public SupportMail()
+        {
+            senderMail = "aljuvifoods@gmail.com";
+            password = "aljuvi1234";
+            host = "smtp.gmail.com";
+            port = 587;
+            ssl = true;
+            InitSmtp();
+        }
+    }
+}
